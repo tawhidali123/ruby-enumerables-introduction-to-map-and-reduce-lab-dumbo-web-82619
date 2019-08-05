bea6065cffdc9913f6485aaab0bfaa7c65863007
Tawhid Ali <tawhidali126@gmail.com>
2019-08-05 20:34:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,13 +35,7 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
- source_array.reduce(0) {
-   |value|
-   if value = false
-     return false
-   end
-  
- }
+ source_array.any? 
  
  
   # i = 0
