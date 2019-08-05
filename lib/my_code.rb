7d5f3afd0b3429d828e18dadf862bfc8f20397eb
Tawhid Ali <tawhidali126@gmail.com>
2019-08-05 20:08:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,15 +35,15 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
-  i = 0
-  while i < source_array.length
-  if source_array[i] = true
-    return true
-  elsif source_array[i] = false
-    return false
-  end  
-  i += 1
-  end
+  # i = 0
+  # while i < source_array.length
+  # if source_array[i] = true
+  #   return true
+  # elsif source_array[i] = false
+  #   return false
+  # end  
+  # i += 1
+  # end
   
 end
 
